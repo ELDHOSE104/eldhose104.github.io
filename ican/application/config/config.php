d92a5c80444d0d18a9a1b3541f62aa2ce9120ed5
DelnaDominic <delnadominic93@gmail.com>
2019-08-29 10:09:27 +0530

    config change

--- ican/application/config/config.php
+++ ican/application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://'.$_SERVER['HTTP_HOST'].'/eldhose104.github.io/ican';
+// $config['base_url'] = 'https://'.$_SERVER['HTTP_HOST'].'/eldhose104.github.io/ican';
+$config['base_url'] = 'https://'.$_SERVER['HTTP_HOST'].'/ican';
 
 /*
 |--------------------------------------------------------------------------
